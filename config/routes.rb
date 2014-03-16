42cc8d9159301aa5edc892f38a9096d0a59268b1
liz rush <liz@lizmrush.com>
2014-03-16 14:57:57 -0700

    setting on timezone and headers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ DigistoreApi::Application.routes.draw do
 
   resources :products, :only => [:index, :show, :create], :defaults => { :format => 'json' }
 
+  match '*all' => 'application#set_access_control_headers', via: :options
 end
